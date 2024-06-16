60647088eda7ee09d332b3836453b6a1811b2a98
IsaelGabriel <isaelgsfaria@gmail.com>
2024-06-16 01:40:51 -0300

    Add rotation to player

--- src/Game/Entities/Player.cs
+++ src/Game/Entities/Player.cs
@@ -1,5 +1,4 @@
 using Raylib_cs;
-using System.ComponentModel.DataAnnotations;
 using System.Numerics;
 public class Player : IPositionable, IRenderable, IUpdatable
 {
@@ -7,6 +6,8 @@ public class Player : IPositionable, IRenderable, IUpdatable
     private const int UpdatePriority = 0;
     private const float BaseSpeed = 200f;
     private Vector2 _position = new Vector2(400, 400);
+    private Vector2 _size = new Vector2(32, 32);
+    private float _rotationDegrees = 0f;
 
     #region IPositionable
         public Vector2 GetPosition()
@@ -31,7 +32,9 @@ public class Player : IPositionable, IRenderable, IUpdatable
 
         public void Render()
         {
-            Raylib.DrawRectangle((int) this.GetPosition().X, (int) this.GetPosition().Y, 32, 32, Color.Gold);
+            Rectangle rec = new Rectangle(_position, _size);
+            //Raylib.DrawRectanglePro(rec, _size * 0.5f, _rotationDegrees * 360f, Color.Gold);
+            Raylib.DrawPoly(_position + _size/2, 3, _size.Length() / 2, _rotationDegrees * 180f / (float) Math.PI, Color.Gold);
         }
 
         public bool Visible()
@@ -43,13 +46,23 @@ public class Player : IPositionable, IRenderable, IUpdatable
     #region IUpdatable
         public void Update()
         {
+            // Move
             Vector2 positionDifference = new Vector2(0, 0);
+            
             positionDifference.X = Raylib.IsKeyDown(KeyboardKey.D) - Raylib.IsKeyDown(KeyboardKey.A);
             positionDifference.Y = Raylib.IsKeyDown(KeyboardKey.S) - Raylib.IsKeyDown(KeyboardKey.W);
+            
             if(positionDifference.Length() != 0)
                 positionDifference = Vector2.Normalize(positionDifference);
 
             ((IPositionable)this).TranslatePosition(positionDifference * Raylib.GetFrameTime() * BaseSpeed);
+
+            // Rotate
+            Vector2 mousePosition = Raylib.GetMousePosition();
+            Vector2 screenPosition = Raylib.GetWorldToScreen2D(_position + _size/2, Engine.Camera);
+            Vector2 rotationVector = -Vector2.Normalize(screenPosition - mousePosition);
+
+            _rotationDegrees = (float) Math.Atan2(rotationVector.Y, rotationVector.X);
         }
 
         public int GetUpdatePriority()
