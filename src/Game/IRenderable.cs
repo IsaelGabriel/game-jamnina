beb2ed734698d870fb4631db170b23c5eea9a49e
IsaelGabriel <isaelgsfaria@gmail.com>
2024-06-16 03:23:53 -0300

    Fix RenderLayer compare

--- src/Game/IRenderable.cs
+++ src/Game/IRenderable.cs
@@ -4,7 +4,7 @@ public interface IRenderable {
     public bool Visible();
 
     public int CompareRenderLayerTo(IRenderable renderable) {
-        int difference = (int) this.renderLayer - (int) renderable.renderLayer;
+        int difference = (int) renderable.renderLayer - (int) this.renderLayer;
         if(difference > 0) return 1;
         if(difference < 0) return -1;
         return 0;
