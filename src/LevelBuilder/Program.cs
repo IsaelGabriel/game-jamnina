1cf4f7206314ecf8c82534802fed8d1dd0cbc493
IsaelGabriel <isaelgsfaria@gmail.com>
2024-06-18 04:42:36 -0300

    Add LevelBuilder project

---
+++ src/LevelBuilder/Program.cs
@@ -0,0 +1,14 @@
+﻿public static class LevelBuilder {
+    private const int tileSize = 32;
+    private const int levelCellsLength = 16;
+
+
+
+    static void Main() {
+        
+    }
+
+    static void Render() {
+
+    }
+}
